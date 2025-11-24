8751576f26085b49ec7f705cd28dc93e22d25ebf
Ashu87289 <ashutosh87289@gmail.com>
2025-11-24 21:55:18 +0530

    First Commmit

---
+++ Ecommerce/src/main/java/com/ashu/Ecommerce/configuration/RestTemplateConfig.java
@@ -0,0 +1,28 @@
+package com.ashu.Ecommerce.configuration;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.http.HttpClient;
+import java.time.Duration;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate(){
+
+        HttpClient httpClient = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofMillis(1000))
+                .build();
+
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(2000);
+        factory.setReadTimeout(2000);
+        return new RestTemplate(factory);
+
+    }
+}
--- Ecommerce/src/main/java/com/ashu/Ecommerce/gateway/FakeStoreRestTemplateGateway.java
+++ Ecommerce/src/main/java/com/ashu/Ecommerce/gateway/FakeStoreRestTemplateGateway.java
@@ -1,21 +1,57 @@
 package com.ashu.Ecommerce.gateway;
 
+import com.ashu.Ecommerce.configuration.RestTemplateConfig;
 import com.ashu.Ecommerce.dto.CartDTO;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 @Component("fakeStoreRestTemplateGateway")
+@AllArgsConstructor
 public class FakeStoreRestTemplateGateway implements ICartGateway{
+
+    private final RestTemplate restTemplate;
 
     @Override
     public List<CartDTO> getAllCarts() throws IOException {
-        return List.of();
+        String url = "https://fakestoreapi.com/carts";
+        System.out.println("URL : " + url);
+        try {
+            ResponseEntity<CartDTO[]> response = restTemplate.getForEntity(url, CartDTO[].class);
+
+            CartDTO[] body = response.getBody();
+
+            if (response.getBody() == null  ) {
+                throw new IOException("Failed to fetch the Carts Data.");
+            }
+            return Arrays.asList(body);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IOException("Error fetching carts: " + e.getMessage(), e);
+        }
+
     }
 
     @Override
     public CartDTO getCartById(int id) throws IOException {
-        return null;
+        String url = "https://fakestoreapi.com/carts/" + id;
+        try{
+            CartDTO response = restTemplate.getForEntity(url,CartDTO.class).getBody();
+            if(response == null){
+                throw new IOException("Cart Not found!");
+            }
+            return response;
+
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new IOException("failed to fetch the data for the given id : " + id);
+        }
     }
 }
